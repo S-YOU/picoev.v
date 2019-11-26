872ee18c7e204681b239c254b454fd2187c05aca
Alexander Medvednikov <alexander@medvednikov.com>
2019-11-26 14:27:09 +0300

    make it compile with the latest V

--- picoev.v
+++ picoev.v
@@ -13,6 +13,7 @@ import net
 #flag @VMOD/syou/picoev/picoev.o
 #include "src/picoev.h"
 
+/*
 import const (
 	PICOEV_READ
 	PICOEV_TIMEOUT
@@ -33,6 +34,7 @@ import const (
 
 	NULL
 )
+*/
 
 const (
 	MaxFds = 1024
@@ -50,10 +52,22 @@ struct Picoev {
 	cb fn(res byteptr) string
 }
 
+fn C.fcntl() int
+fn C.picoev_del() int
+fn C.picoev_set_timeout()
+fn C.picoev_add()
+fn C.write() int
+fn C.picoev_init() int
+fn C.picoev_deinit() int
+fn C.picoev_create_loop() int
+fn C.picoev_loop_once() int
+fn C.picoev_destroy_loop() int
+//fn C.accept() int
+
 pub fn setup_sock(fd int) {
 	on := int(1)
-	C.setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, &on, sizeof(int))
-	C.fcntl(fd, F_SETFL, O_NONBLOCK) == 0
+	C.setsockopt(fd, C.IPPROTO_TCP, C.TCP_NODELAY, &on, sizeof(int))
+	C.fcntl(fd, C.F_SETFL, C.O_NONBLOCK) == 0
 }
 
 pub fn close_conn(loop *C.picoev_loop, fd int) {
@@ -62,22 +76,24 @@ pub fn close_conn(loop *C.picoev_loop, fd int) {
 }
 
 fn rw_callback(loop *C.picoev_loop, fd, events int, cb_arg voidptr) {
-	if (events & PICOEV_TIMEOUT) != 0 {
+	if (events & C.PICOEV_TIMEOUT) != 0 {
 		close_conn(loop, fd)
-	} else if (events & PICOEV_READ) != 0 {
+	} else if (events & C.PICOEV_READ) != 0 {
 		buf := [1536]byte
-		C.picoev_set_timeout(loop, fd, TIMEOUT_SECS)
+		C.picoev_set_timeout(loop, fd, C.TIMEOUT_SECS)
 		r := C.read(fd, buf, 1024)
-		switch (r) {
-		case 0:
+		match r {
+		0 {
 			close_conn(loop, fd)
-		case -1:
-			if errno == EAGAIN || errno == EWOULDBLOCK {
+		}
+		-1 {
+			if errno == C.EAGAIN || errno == C.EWOULDBLOCK {
 				//
 			} else {
 				close_conn(loop, fd)
 			}
-		default:
+		}
+		else {
 			p := *Picoev(cb_arg)
 			cb := p.cb
 			res := cb(buf)
@@ -89,30 +105,32 @@ fn rw_callback(loop *C.picoev_loop, fd, events int, cb_arg voidptr) {
 				close_conn(loop, fd)
 			}
 		}
+		}
 	}
 }
 
 fn accept_callback(loop *C.picoev_loop, fd, events int, cb_arg voidptr) {
-	newfd := int(C.accept(fd, NULL, NULL))
+	newfd := C.accept(fd, 0, 0)
 	if newfd != -1 {
 		setup_sock(newfd)
-		C.picoev_add(loop, newfd, PICOEV_READ, TIMEOUT_SECS, rw_callback, cb_arg)
+		C.picoev_add(loop, newfd, C.PICOEV_READ, C.TIMEOUT_SECS,
+			rw_callback, cb_arg)
 	}
 }
 
-pub fn new(port int, cb voidptr) *Picoev {
-	sock := net.socket(AF_INET, SOCK_STREAM, 0) or { panic(err) }
+pub fn new(port int, cb voidptr) &Picoev {
+	sock := net.new_socket(C.AF_INET, C.SOCK_STREAM, 0) or { panic(err) }
 	assert sock.sockfd != -1
 
 	flag := int(1)
-	assert C.setsockopt(sock.sockfd, SOL_SOCKET, SO_REUSEADDR, &flag, 8) == 0
-	assert C.setsockopt(sock.sockfd, SOL_SOCKET, SO_REUSEPORT, &flag, 8) == 0
+	assert C.setsockopt(sock.sockfd, C.SOL_SOCKET, C.SO_REUSEADDR, &flag, 8) == 0
+	assert C.setsockopt(sock.sockfd, C.SOL_SOCKET, C.SO_REUSEPORT, &flag, 8) == 0
 	$if linux {
-		assert C.setsockopt(sock.sockfd, IPPROTO_TCP, TCP_QUICKACK, &flag, 8) == 0
+		assert C.setsockopt(sock.sockfd, C.IPPROTO_TCP, C.TCP_QUICKACK, &flag, 8) == 0
 		timeout := int(10)
-		assert C.setsockopt(sock.sockfd, IPPROTO_TCP, TCP_DEFER_ACCEPT, &timeout, 8) == 0
+		assert C.setsockopt(sock.sockfd, C.IPPROTO_TCP, C.TCP_DEFER_ACCEPT, &timeout, 8) == 0
 		queue_len := int(4096)
-		assert C.setsockopt(sock.sockfd, IPPROTO_TCP, TCP_FASTOPEN, &queue_len, 8) == 0
+		assert C.setsockopt(sock.sockfd, C.IPPROTO_TCP, C.TCP_FASTOPEN, &queue_len, 8) == 0
 	}
 
 	bind_res := sock.bind(port) or { panic(err) }
@@ -130,7 +148,7 @@ pub fn new(port int, cb voidptr) *Picoev {
 		loop: loop,
 		cb: cb
 	}
-	C.picoev_add(loop, sock.sockfd, PICOEV_READ, 0, accept_callback, picoev)
+	C.picoev_add(loop, sock.sockfd, C.PICOEV_READ, 0, accept_callback, picoev)
 
 	return picoev
 }
